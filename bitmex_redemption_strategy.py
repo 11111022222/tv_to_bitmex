7cd3277765ff428ec3e76a85f94eab253795e47e
Victor Troussel <53226388+victor-troussel@users.noreply.github.com>
2020-08-05 10:12:25 +0200

    Update bitmex_redemption_strategy.py

--- bitmex_redemption_strategy.py
+++ bitmex_redemption_strategy.py
@@ -1,19 +1,3 @@
-import bitmex_redemption.bitmex_redemption_strategy as redemption_strategy #à remplacer par votre script qui executera la strategie
-REDEMPTION_STRATEGY_LONG = "long_redemption"
-REDEMPTION_STRATEGY_STOPLONG = "stop_long_redemption"
-REDEMPTION_STRATEGY_SHORT = "short_redemption"
-REDEMPTION_STRATEGY_STOPSHORT = "stop_short_redemption"
-def call_strategy(email_snippet):
-#calling redemption strategy
-    if REDEMPTION_STRATEGY_STOPLONG in email_snippet:
-        redemption_strategy.stop_long()
-    elif REDEMPTION_STRATEGY_LONG in email_snippet:
-        redemption_strategy.long()
-    elif REDEMPTION_STRATEGY_SHORT in email_snippet:
-        redemption_strategy.short()
-    elif REDEMPTION_STRATEGY_STOPSHORT in email_snippet:
-        redemption_strategy.stop_short()
-Script executant la stratégie :
 import bitmex, datetime, sys, warnings
 warnings.filterwarnings("ignore")
 BITMEX_API_KEY = '' #remplacer par vos clés API
